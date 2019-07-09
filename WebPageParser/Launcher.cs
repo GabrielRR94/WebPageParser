74444063c88d474d9e5690c29e794ecf726ec689
Gabriel Rodríguez Ríos <grodriguez@fonetic.local>
2019-07-09 09:56:01 +0200

    Modifications by Lozano to make it prettier.

--- WebPageParser/Launcher.cs
+++ WebPageParser/Launcher.cs
@@ -20,7 +20,7 @@ namespace WebPageParser.cs
         private readonly ICsvHandler _csvHandler;
         private readonly string CsvName = $"{DateTime.UtcNow.ToString("yyyyMMdd")}.csv";
 
-        private Regex _regex = new Regex("(.)(?<=\\1\\1\\1)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+        private Regex _regex = new Regex(@"(.)(?<=\1\1\1)", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
         public Launcher(IArgumentsParser argumentsParser, IURLGenerator URLGenerator,ICsvHandler csvHandler)
         {
             _argumentsParser = argumentsParser;
@@ -38,48 +38,37 @@ namespace WebPageParser.cs
             {
                 HtmlDocument document = htmlWeb.Load(urlToVisit);
                 HtmlNode tableHeaderNode = document.DocumentNode.SelectSingleNode("//*[@id='tablaranking']/thead/tr");
-                var tableHeader = CleanRecord(tableHeaderNode.InnerText);
+                var tableHeader = BaseClean(tableHeaderNode.InnerText);
                 //tableHeader ready to write on the csv
                 _csvHandler.WriteSingleRecord(CsvName, tableHeader);
 
                 HtmlNode tableNode = document.DocumentNode.SelectSingleNode("//*[@id='tablaranking']/tbody");
                 var tableNodes = tableNode.ChildNodes;
 
-                foreach (var _tableNode in tableNodes)
+                var recordsTowrite = tableNodes.Select(n => CleanRow(n.InnerText)).Where(r => !string.IsNullOrWhiteSpace(r));
+
+                foreach(var row in recordsTowrite)
                 {
-                    var tableRowData = CleanRecord(_tableNode.InnerText);
-                    //tableRowData ready to store the record of the data
-                    _csvHandler.CreateRecords(tableRowData);
+                    _csvHandler.CreateRecords(row);
                 }
             }
             _csvHandler.WriteCsvRecords(CsvName);
         }
-
-        private string RemoveWhitespace(string input)
+        
+        private string BaseClean(string rawRecord)
         {
-            return new string(input.ToCharArray()
-                .Where(c => !char.IsWhiteSpace(c))
-                .ToArray());
+            var headers = rawRecord.Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Where(x => !string.IsNullOrWhiteSpace(x));
+            var toClean = string.Join(",", headers);
+            var rgx = new Regex(@"\s+");
+           return rgx.Replace(toClean, string.Empty);
         }
 
-        private string CleanRecord(string rawRecord)
+        private string CleanRow(string rawRecord)
         {
-            rawRecord = rawRecord.Replace("\t", string.Empty);
-            rawRecord = rawRecord.Replace("\r\n", ",");
-            rawRecord = RemoveWhitespace(rawRecord);
-            rawRecord = _regex.Replace(rawRecord, string.Empty);
-            rawRecord = rawRecord.Replace(",,", ",");
-            string record = string.Empty;
-            try
-            {
-                rawRecord = rawRecord.Remove(0, 1);
-                record = rawRecord.Remove(rawRecord.Length - 1, 1);
-            }
-            catch (Exception)
-            {
-                //do nothing in case of error, suicide coming up!!!!
-            }
-            return record;
+            var cleaned = BaseClean(rawRecord);
+            var rgx = new Regex(@"(\d+),\(\1\)");
+            var match = rgx.Match(cleaned);
+            return match.Success ? $"{match.Groups[1]}{cleaned.Substring(match.Index + match.Length)}" : cleaned;
         }
     }
 }
